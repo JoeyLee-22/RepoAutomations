d5c712c2b966c1ab68b86299466c39b390c569bb
Joey Lee <66647435+JoeyLee-22@users.noreply.github.com>
2020-09-20 10:08:51 -0700

    inital commit

---
+++ .my_commands.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+alias bash_profile='open ~/.bash_profile'
+alias commands='open ~/Documents/GitHubProjects/RepoAutomations/.my_commands.sh'
+
+function create() {
+  cd Documents/GitHubProjects
+  mkdir $1
+  cd
+  python3 Documents/GitHubProjects/RepoAutomations/create.py $1
+  cd Documents/GitHubProjects/$1
+  git init
+  git remote add origin https://github.com/JoeyLee-22/$1.git
+  touch README.md
+  git add .
+  git commit -m "Initial commit"
+  git push -u origin master
+  code .
+}
+
+function delete() {
+  cd
+  python3 Documents/GitHubProjects/RepoAutomations/delete.py $1
+  cd Documents/GitHubProjects
+  rm -fr $1
+}
+
+function goto() {
+  cd
+  cd Documents/GitHubProjects/$1
+}
