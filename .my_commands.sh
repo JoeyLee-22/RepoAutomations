cbdfe81dc42f063ec92b0d67e0217fa0a8d24560
Joey Lee <66647435+JoeyLee-22@users.noreply.github.com>
2020-10-29 22:25:17 -0700

    less code for file pushing

--- .my_commands.sh
+++ .my_commands.sh
@@ -44,40 +44,24 @@ function goto() {
 
 function gp() {
   ask_push=false
+  auto_message_bool=false
   if test -f "$1"; then
     file1="README.md"
     file2=".gitignore"
     if [[ "$1" == "$file1" ]]; then
-      git add $1
-      while true; do
-        read -p "Auto Message? (y/n) " yn
-        if [[ "$yn" == "y" ]]; then
-          git commit -m "update README.md"
-          while true; do
-            read -p "Push? (y/n) " yn
-            if [[ "$yn" == "y" ]]; then
-              git push origin master
-              break
-            elif [[ "$yn" == "n" ]]; then
-              git reset HEAD~
-              break
-            fi
-          done
-          break
-        elif [[ "$yn" == "n" ]]; then
-          read -p "Enter Commit Summary: " message
-          git add $1
-          git commit -m "$message"
-          ask_push=true
-          break
-        fi
-      done
+      auto_message="update README.md"
+      auto_message_bool=true
     elif [[ "$1" == "$file2" ]]; then
+      auto_message="update .gitignore"
+      auto_message_bool=true
+    fi
+
+    if $auto_message_bool; then
       git add $1
       while true; do
         read -p "Auto Message? (y/n) " yn
         if [[ "$yn" == "y" ]]; then
-          git commit -m "update .gitignore"
+          git commit -m "$auto_message"
           while true; do
             read -p "Push? (y/n) " yn
             if [[ "$yn" == "y" ]]; then
@@ -103,11 +87,13 @@ function gp() {
       git commit -m "$message"
       ask_push=true
     fi
+    
   elif test -d "$1"; then
     read -p "Enter Commit Summary: " message
     git add $1
     git commit -m "$message"
     ask_push=true
+
   elif !$ask_push; then
     echo "$1: No such file or directory"
   fi
