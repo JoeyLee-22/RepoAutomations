ace85e52ea80421a713fc01b7280261a370bb077
Joey Lee <66647435+JoeyLee-22@users.noreply.github.com>
2020-10-31 10:05:18 -0700

    bug fix

--- create.py
+++ create.py
@@ -8,15 +8,15 @@ browser = webdriver.Chrome('/Users/joeylee/chromedriver')
 browser.get('https://www.github.com/login')
 
 login_form = browser.find_element_by_id("login_field")
-login_form.send_keys(open(r'email.txt','r').read())
+login_form.send_keys(open(r'/Users/joeylee/Documents/GitHubProjects/RepoAutomations/email.txt','r').read())
 password_form = browser.find_element_by_id("password")
-password_form.send_keys(open(r'password.txt','r').read())
+password_form.send_keys(open(r'/Users/joeylee/Documents/GitHubProjects/RepoAutomations/password.txt','r').read())
 
-login_button = browser.find_element_by_xpath('//*[@id="login"]/form/div[4]/input[9]').click()
+login_button = browser.find_element_by_name('commit').click()
 
 browser.get('https://www.github.com/new')
 
-repository_name_form = browser.find_element_by_xpath('/html/body/div[4]/main/div/form/div[2]/auto-check/dl/dd/input')
+repository_name_form = browser.find_element_by_name('repository[name]')
 repository_name_form.send_keys(repository_name)
 
 private_button = browser.find_element_by_xpath('//*[@id="repository_visibility_private"]').click()
