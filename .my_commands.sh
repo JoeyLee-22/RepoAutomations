9f49c8f32e2cf5f4cdb31d93e13fa989caa87677
Joey Lee <66647435+JoeyLee-22@users.noreply.github.com>
2020-11-13 17:39:10 -0800

    delete code that commits README file automatically

--- .my_commands.sh
+++ .my_commands.sh
@@ -14,10 +14,6 @@ function create() {
   cd Documents/GitHubProjects/$1
   git init
   git remote add origin https://github.com/JoeyLee-22/$1.git
-  touch README.md
-  git add .
-  git commit -m "Initial commit"
-  git push -u origin master
   code .
 }
 
