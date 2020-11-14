a011500b9c10711ed9a3bb6ff44e313db0e1c82b
Joey Lee <66647435+JoeyLee-22@users.noreply.github.com>
2020-11-13 17:38:27 -0800

    be able to add gitignore file, choose whether private or not, and be able to choose whether you create a README or not

--- create.py
+++ create.py
@@ -4,6 +4,31 @@ import time
 
 repository_name = str(sys.argv[1])
 
+while True:
+    user_input = input('Private? (y/n): ').lower()
+    if  user_input == 'n':
+        private = False
+        break
+    elif user_input == 'y':
+        private = True
+        break
+while True:
+    user_input = input('Make Gitignore File? (y/n): ').lower()
+    if  user_input == 'n':
+        gitignore = False
+        break
+    elif user_input == 'y':
+        gitignore = True
+        break
+while True:
+    user_input = input('Make README File? (y/n): ').lower()
+    if  user_input == 'n':
+        readme = False
+        break
+    elif user_input == 'y':
+        readme = True
+        break
+
 browser = webdriver.Chrome('/Users/joeylee/chromedriver')
 browser.get('https://www.github.com/login')
 
@@ -19,10 +44,14 @@ browser.get('https://www.github.com/new')
 repository_name_form = browser.find_element_by_name('repository[name]')
 repository_name_form.send_keys(repository_name)
 
-private_button = browser.find_element_by_xpath('//*[@id="repository_visibility_private"]').click()
+if private:
+    private_button = browser.find_element_by_id('repository_visibility_private').click()
+if gitignore:
+    gitignore_button = browser.find_element_by_id('repository_gitignore_template_toggle').click()
+if readme:
+    readme_button = browser.find_element_by_id('repository_auto_init').click()
 
 time.sleep(0.5)
 
 create_button = browser.find_element_by_xpath('//*[@id="new_repository"]/div[4]/button').click()
-
-browser.close()
+# browser.close()
